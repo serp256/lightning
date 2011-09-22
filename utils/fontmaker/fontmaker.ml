96f639606b46f68c34a6e3baafc84e6bda449167
Sergey Plaksin <serp256@gmail.com>
2011-09-23 00:01:39 +0400

    fontmaker working

--- utils/fontmaker/fontmaker.ml
+++ utils/fontmaker/fontmaker.ml
@@ -1,4 +1,5 @@
 open Printf;
+open ExtString;
 
 module Ft = Freetype;
 
@@ -12,9 +13,9 @@ value (=|=) k v = (("",k),v);
 value (=.=) k v = k =|= string_of_float v;
 value (=*=) k v = k =|= string_of_int v;
 
-value pattern = ref "abcdefghабвг";
-value fontFile = ref "DejaVuSerif.ttf";
-value sizes = ref [ 24. ; 100.0 ]; (* add support for multisize *)
+value pattern = ref "";
+value fontFile = ref "";
+value sizes = ref [ ]; (* add support for multisize *)
 value color = ref {Color.r = 255; g = 255; b = 255};
 value dpi = ref 72;
 
@@ -26,19 +27,20 @@ value make_size face size callback =
   UTF8.iter begin fun uchar ->
   (
     let code = UChar.code uchar in
-    let () = Printf.printf "process char: %d\n%!" code in
+(*     let () = Printf.printf "process char: %d\n%!" code in *)
     let char_index = Freetype.get_char_index face code in
     let (xadv,yadv) = Freetype.render_glyph face char_index [] Freetype.Render_Normal in
     let bi = Freetype.get_bitmap_info face in
     (* take bitmap as is for now *)
     let open Freetype in
-    let () = Printf.printf "bi.width: %d, bi.height: %d\n%!" bi.bitmap_width bi.bitmap_height in
+(*     let () = Printf.printf "bi.width: %d, bi.height: %d\n%!" bi.bitmap_width bi.bitmap_height in *)
     let img =  Rgba32.make bi.bitmap_width bi.bitmap_height bgcolor in
     (
       for y = 0 to bi.bitmap_height - 1 do
         for x = 0 to bi.bitmap_width - 1 do
           let level = read_bitmap face x y in
-          Rgba32.set img x (bi.bitmap_height - y - 1) {Color.color = !color; alpha = level}
+          let color = {Color.color = {Color.r=level;Color.g=level;Color.b=level}; alpha = level} in
+          Rgba32.set img x (bi.bitmap_height - y - 1) color
         done
       done;
 (*         img#save (Printf.sprintf "%d.png" code) (Some Images.Png) []; *)
@@ -50,40 +52,45 @@ value make_size face size callback =
 
 type sdescr = 
   {
-    id: int; xadvance: int; xoffset: int; yoffset: int; 
+    id: int; xadvance: float; xoffset: int; yoffset: int; 
     width: int; height: int; x: mutable int; y: mutable int; page: mutable int
   };
 
+
+value parse_sizes str = 
+  try
+    sizes.val := List.map float_of_string (String.nsplit str ",")
+  with [ _ -> failwith "Failure parse sizes" ];
+
+value read_chars fname = pattern.val := String.strip (Std.input_file fname);
+
 (* use xmlm for writing xml *)
+Arg.parse 
+  [
+    ("-c",Arg.Set_string pattern,"chars");
+    ("-s",Arg.String parse_sizes,"sizes");
+    ("-cf",Arg.String read_chars,"chars from file")
+  ] 
+  (fun f -> fontFile.val := f) "Usage msg";
+
+value bad_arg what = (Printf.printf "bad argument '%s'\n%!" what; exit 1);
+if !pattern = "" 
+then bad_arg "chars"
+else 
+  if !sizes = [] then bad_arg "sizes"
+  else 
+    if !fontFile = "" then bad_arg "font file"
+    else ();
+
+Printf.printf "chars: [%s] = %d\n%!" !pattern (UTF8.length !pattern);
 let t = Freetype.init () in
 let (face,face_info) = Freetype.new_face t !fontFile 0 in
 let chars = Hashtbl.create 1 in
+let fname = Filename.chop_extension !fontFile in
+let xmlfname = fname ^ ".fnt" in
+let out = open_out xmlfname in
+let xmlout = Xmlm.make_output (`Channel (open_out xmlfname)) in
 (
-  let imgs = ref [] in
-  (
-    List.iter begin fun size ->
-      make_size face size begin fun code xadvance xoffset yoffset img ->
-        imgs.val := [ ((size,code),Images.Rgba32 img) :: !imgs ]
-      end
-    end !sizes;
-    let () = Printf.printf "len imgs: %d\n%!" (List.length !imgs) in
-    let textures = TextureLayout.layout !imgs in
-    (* save output *)
-    ExtList.List.iteri begin fun i (w,h,imgs) ->
-      let texture = Rgba32.make w h bgcolor in
-      (
-        List.iter begin fun ((size,code),(x,y,img)) ->
-          let img = match img with [ Images.Rgba32 img -> img | _ -> assert False ] in
-          Rgba32.blit img 0 0 texture x y img.Rgba32.width img.Rgba32.height;
-        end imgs;
-        Images.save (Printf.sprintf "%d.png" i) (Some Images.Png) [] (Images.Rgba32 texture);
-      )
-    end textures;
-  );
-  (*
-  let xmlfname = (Filename.chop_extension !fontFile) ^ ".fnt" in
-  let xmlout = Xmlm.make_output (`Channel (open_out xmlfname)) in
-  (
   Xmlm.output xmlout (`Dtd None);
   print_face_info face_info;
   let fattribs = 
@@ -93,8 +100,69 @@ let chars = Hashtbl.create 1 in
     ]
   in
   Xmlm.output xmlout (`El_start (("","Font"),fattribs));
-  Xmlm.output xmlout (`El_start (("","Chars"),[ "size" =.= size ];
-  *)
+  let imgs = ref [] in
+  (
+    List.iter begin fun size ->
+      make_size face size begin fun code xadvance xoffset yoffset img ->
+        let key = (code,size) in
+        (
+          imgs.val := [ (key,Images.Rgba32 img) :: !imgs ];
+          Hashtbl.add chars key {id=code;xadvance;xoffset;yoffset;width=img.Rgba32.width;height=img.Rgba32.height;x=0;y=0;page=0};
+        )
+      end
+    end !sizes;
+    let () = Printf.printf "len imgs: %d\n%!" (List.length !imgs) in
+    Xmlm.output xmlout (`El_start (("","Pages"),[]));
+    let textures = TextureLayout.layout !imgs in
+    ExtList.List.iteri begin fun i (w,h,imgs) ->
+      let texture = Rgba32.make w h bgcolor in
+      (
+        List.iter begin fun (key,(x,y,img)) ->
+          (
+            let img = match img with [ Images.Rgba32 img -> img | _ -> assert False ] in
+            Rgba32.blit img 0 0 texture x y img.Rgba32.width img.Rgba32.height;
+            let r = Hashtbl.find chars key in
+            ( r.x := x; r.y := y; r.page := i;)
+          )
+        end imgs;
+        let imgname = Printf.sprintf "%s%d.png" fname i in
+        (
+          Images.save imgname (Some Images.Png) [] (Images.Rgba32 texture);
+          Xmlm.output xmlout (`El_start (("","Page"),["file" =|= imgname]));
+          Xmlm.output xmlout `El_end;
+        );
+      )
+    end textures;
+    Xmlm.output xmlout `El_end;
+  );
+  List.iter begin fun size ->
+    (
+      Xmlm.output xmlout (`El_start (("","Chars"),[ "size" =.= size ]));
+      UTF8.iter begin fun uchar ->
+        let code = UChar.code uchar in
+        let info = Hashtbl.find chars (code,size) in
+        let attribs = 
+          [ "id" =*= code
+          ; "xadvance" =.= info.xadvance
+          ; "xoffset" =*= info.xoffset
+          ; "yoffset" =*= info.yoffset
+          ; "x" =*= info.x
+          ; "y" =*= info.y
+          ; "width" =*= info.width
+          ; "height" =*= info.height
+          ; "page" =*= info.page
+          ]
+        in
+        (
+          Xmlm.output xmlout (`El_start (("","char"),attribs));
+          Xmlm.output xmlout `El_end;
+        )
+      end !pattern;
+      Xmlm.output xmlout `El_end;
+    )
+  end !sizes;
+  Xmlm.output xmlout `El_end;
+  close_out out;
 );
 
 
