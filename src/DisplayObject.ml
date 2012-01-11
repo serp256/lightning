10710cccc51bf15052c44feee708db486fde2c44
Sergey Plaksin <serp256@gmail.com>
2012-01-11 18:34:25 +0300

    added setChildIndex - not tested

--- src/DisplayObject.ml
+++ src/DisplayObject.ml
@@ -755,6 +755,66 @@ class virtual container = (*{{{*)
             [ Not_found -> raise Child_not_found ] 
       ];
 
+    method setChildIndex: !'child. ((#_c container) as 'child) -> int -> unit = fun child index -> 
+      match children with
+      [ None -> raise Child_not_found
+      | Some chldrn ->
+          if index >= numChildren || index < 0 then raise Invalid_index
+          else
+            let child = child#asDisplayObject in
+            match Dllist.get chldrn = child with
+            [ True -> 
+              let next_node = Dllist.next chldrn in
+              match next_node == chldrn with
+              [ True -> () (* ничего делать не надо *)
+              | False -> 
+                  let prev = Dllist.skip next_node (index - 1) in
+                  let next = Dllist.next prev in
+                  (
+                    Dllist.splice prev chldrn;
+                    Dllist.splice chldrn next;
+                    children := Some next_node;
+                  )
+              ]
+            | False -> 
+                let node = find_node (Dllist.next chldrn) where
+                  rec find_node n =
+                    if n != chldrn
+                    then 
+                      match Dllist.get n = child with
+                      [ True -> n
+                      | False -> find_node (Dllist.next n)
+                      ]
+                    else raise Child_not_found
+                in
+                (
+                  Dllist.remove node;
+                  if index = 0
+                  then
+                  (
+                    children := Some node;
+                    Dllist.splice (Dllist.prev chldrn) node;
+                    Dllist.splice node chldrn;
+                    children := Some node
+                  )
+                  else
+                    if index = numChildren - 1
+                    then
+                    (
+                      Dllist.splice (Dllist.prev chldrn) node;
+                      Dllist.splice node chldrn;
+                    )
+                    else
+                      let prev = Dllist.skip chldrn (index - 1) in
+                      let next = Dllist.next prev in
+                      (
+                        Dllist.splice prev node;
+                        Dllist.splice node next;
+                      )
+                )
+            ]
+      ];
+
     (* FIXME: защиту от зацикливаний бы поставить нах *)
     method private removeChild'' (child_node:Dllist.node_t 'displayObject) =
       let child = Dllist.get child_node in
--- src/DisplayObjectT.ml
+++ src/DisplayObjectT.ml
@@ -114,6 +114,7 @@ class virtual container:
     method addChild: !'child. ?index:int -> (#_c container as 'child) -> unit;
     method containsChild: !'child. (#_c container as 'child) -> bool;
     method getChildAt: int -> 'displayObject;
+    method setChildIndex: !'child. (#_c container as 'child) -> int -> unit;
     method getLastChild: 'displayObject;
     method getChildIndex: !'child. (#_c container as 'child) -> int;
     method numChildren: int;
