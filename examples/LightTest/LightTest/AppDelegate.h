c90be599a6cce2c86551e1c7b238532547996f84
Sergey Plaksin <serp256@gmail.com>
2012-02-06 17:30:23 +0400

    next

---
+++ examples/LightTest/LightTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LightTest
+//
+//  Created by Sergey Plaksin on 2/3/12.
+//  Copyright (c) 2012 RedSpell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
