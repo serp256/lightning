2a18726d7fb935313ecdb0f642a070a92b087467
Sergey Plaksin <serp256@gmail.com>
2012-05-29 16:42:24 +0400

    added Hardware

---
+++ src/Hardware.ml
@@ -0,0 +1,16 @@
+
+
+
+IFDEF IOS THEN
+external platform: unit -> string = "ml_platform";
+external hwmodel: unit -> string = "ml_hwmodel";
+external cpu_frequency: unit -> int = "ml_cpuFrequency";
+external total_memory: unit -> int = "ml_totalMemory";
+external user_memory: unit -> int = "ml_userMemory";
+ELSE
+value platform () = "PLATFORM";
+value hwmodel () = "HWMODEL";
+value cpu_frequency () = 0;
+value total_memory () = 0;
+value user_memory () = 0;
+ENDIF;
---
+++ src/ios/hardware_ios.c
@@ -0,0 +1,49 @@
+#include <sys/sysctl.h>
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+
+
+value ml_platform() {
+  size_t size;
+  sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+	char answer[size];
+  sysctlbyname("hw.machine", answer, &size, NULL, 0);
+  return caml_copy_string(answer);
+}
+
+
+value ml_hwmodel() {
+  size_t size;
+  sysctlbyname("hw.model", NULL, &size, NULL, 0);
+	char answer[size];
+  sysctlbyname("hw.model", answer, &size, NULL, 0);
+  return caml_copy_string(answer);
+}
+
+
+int getSysInfo(int typeSpecifier) {
+  size_t size = sizeof(int);
+  int results;
+  int mib[2] = {CTL_HW, typeSpecifier};
+  sysctl(mib, 2, &results, &size, NULL, 0);
+  return results;
+}
+
+
+value ml_cpuFrequency() {
+  return Val_int(getSysInfo(HW_CPU_FREQ));
+}
+
+
+value ml_busFrequency() {
+  return Val_int(getSysInfo(HW_BUS_FREQ));
+}
+
+value ml_totalMemory() {
+  return Val_int(getSysInfo(HW_PHYSMEM));
+}
+
+value ml_userMemory() {
+  return Val_int(getSysInfo(HW_USERMEM));
+};
+
