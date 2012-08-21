31402c07902267e0588aaa1a3c8b832db989a8df
Esin Alexey <netzs@ya.ru>
2012-08-21 14:32:52 +0400

    добавлены функции копипаста для ios

--- src/Motion.ml
+++ src/Motion.ml
@@ -9,7 +9,12 @@ type callback = acmtrData -> unit;
 IFDEF IOS THEN
 external acmtrStart : callback -> float -> unit = "ml_acmtrStart";
 external acmtrStop : unit -> unit = "ml_acmtrStop";
+external paste : unit -> string = "ml_paste";
+external copy : string -> unit = "ml_copy";
 ELSE
 value acmtrStart (cb:callback) (interval:float) = ();
 value acmtrStop () = ();
+value paste () = "not SDL";
+value copy _ = ();
 ENDIF;
+
