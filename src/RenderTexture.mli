c42818449161de2f8cb1d5264fd61ad00211589e
SerP <serp256@gmail.com>
2012-10-23 15:59:14 +0400

    Render.mli

--- src/RenderTexture.mli
+++ src/RenderTexture.mli
@@ -8,6 +8,7 @@ class type c =
   object
     inherit Texture.c;
     method draw: ?clear:(int*float) -> ?width:float -> ?height:float -> (framebuffer -> unit) -> bool;
+    method texture: Texture.c;
   end;
 
 value draw: ~filter:Texture.filter -> ?color:int -> ?alpha:float -> float ->  float -> (framebuffer -> unit) -> c; 
