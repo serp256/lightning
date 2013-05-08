28b524a71fc45b20d22c5eaa2f06213a6216c38d
SerP <serp256@gmail.com>
2013-05-08 16:03:25 +0400

    MobileAppTracking.mli

---
+++ src/plugins/mobileapptracking/MobileAppTracking.mli
@@ -0,0 +1,5 @@
+
+external init: ~advertiser_id:string -> ~conversion_key:string -> ?site_id:string -> unit -> unit = "ml_MATinit";
+external setUserId: string -> unit = "ml_MATsetUserId";
+external install: unit -> unit = "ml_MATinstall";
+external update: unit -> unit = "ml_MATupdate";
