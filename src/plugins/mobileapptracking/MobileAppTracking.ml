5e69a6b8bff3c1a17b16426473f893694326a10a
SerP <serp256@gmail.com>
2013-05-20 16:09:58 +0400

    Fix MobileAppTracking macos compilation

--- src/plugins/mobileapptracking/MobileAppTracking.ml
+++ src/plugins/mobileapptracking/MobileAppTracking.ml
@@ -1,5 +1,12 @@
-
+IFPLATFORM(ios android)
 external init: ~advertiser_id:string -> ~conversion_key:string -> ?site_id:string -> unit -> unit = "ml_MATinit";
 external setUserId: string -> unit = "ml_MATsetUserId";
 external install: unit -> unit = "ml_MATinstall";
 external update: unit -> unit = "ml_MATupdate";
+ELSE
+value init ~advertiser_id ~conversion_key ?site_id () = ();
+value setUserId _ = ();
+value install () = ();
+value update () = ();
+
+ENDPLATFORM;
--- src/plugins/mobileapptracking/MobileAppTracking.mli
+++ src/plugins/mobileapptracking/MobileAppTracking.mli
@@ -1,5 +1,12 @@
 
+IFPLATFORM(ios android)
 external init: ~advertiser_id:string -> ~conversion_key:string -> ?site_id:string -> unit -> unit = "ml_MATinit";
 external setUserId: string -> unit = "ml_MATsetUserId";
 external install: unit -> unit = "ml_MATinstall";
 external update: unit -> unit = "ml_MATupdate";
+ELSE
+value init: ~advertiser_id:string -> ~conversion_key:string -> ?site_id:string -> unit -> unit;
+value setUserId: string -> unit;
+value install: unit -> unit;
+value update: unit -> unit; 
+ENDPLATFORM;
