9afd7ba6a08eb96d4f42e82b53e62fce6db8ba93
SerP <serp256@gmail.com>
2013-07-29 10:17:08 +0400

    MobileAppTracking added trackAction

--- src/plugins/mobileapptracking/MobileAppTracking.ml
+++ src/plugins/mobileapptracking/MobileAppTracking.ml
@@ -3,10 +3,14 @@ external init: ~advertiser_id:string -> ~conversion_key:string -> ?site_id:strin
 external setUserId: string -> unit = "ml_MATsetUserId";
 external install: unit -> unit = "ml_MATinstall";
 external update: unit -> unit = "ml_MATupdate";
+external trackAction: string -> unit = "ml_MATtrackAction";
+external trackPurchase: ~key:string -> ~amount:float -> ~currency:string -> unit = "ml_MATtrackPurchase";
 ELSE
 value init ~advertiser_id ~conversion_key ?site_id () = ();
 value setUserId _ = ();
 value install () = ();
 value update () = ();
+value trackAction _ = ();
+value trackPurchase ~key ~amount ~currency = ();
 
 ENDPLATFORM;
--- src/plugins/mobileapptracking/ios/mobileapptracker_ios.m
+++ src/plugins/mobileapptracking/ios/mobileapptracker_ios.m
@@ -1,7 +1,9 @@
 
 #import <caml/mlvalues.h>
+#import <caml/fail.h>
 #import <MobileAppTracker/MobileAppTracker.h>
 
+static int initialized = 0;
 
 
 /*
@@ -26,7 +28,8 @@
 */
 
 
-void ml_MATinit(value advertiser_id, value conversion_key, value site_id, value unit) {
+value ml_MATinit(value advertiser_id, value conversion_key, value site_id, value unit) {
+	if (initialized) return Val_unit;
 	NSString *adv = [NSString stringWithCString:String_val(advertiser_id) encoding:NSASCIIStringEncoding];
 	NSString *ck = [NSString stringWithCString:String_val(conversion_key) encoding:NSASCIIStringEncoding];
 	[[MobileAppTracker sharedManager] startTrackerWithMATAdvertiserId:adv MATConversionKey:ck withError:nil];
@@ -34,23 +37,47 @@ void ml_MATinit(value advertiser_id, value conversion_key, value site_id, value 
 		NSString *siteId = [NSString stringWithCString:String_val(Field(site_id,0)) encoding:NSASCIIStringEncoding];
 		[[MobileAppTracker sharedManager] setSiteId:siteId];
 	}
+	initialized = 1;
 	//[[MobileAppTracker sharedManager] setDebugMode:YES];
 	//[[MobileAppTracker sharedManager] setAllowDuplicateRequests:YES];
 	//MyDelegateHandler *matHandler = [[[MyDelegateHandler alloc] init] autorelease];
 	//[[MobileAppTracker sharedManager] setDelegate:matHandler];
+	return Val_unit;
 }
 
 
-void ml_MATsetUserId(value user_id) {
+value ml_MATsetUserId(value user_id) {
+	if (!initialized) caml_failwith("MobileAppTracker not initialized");
 	NSString *uid = [NSString stringWithCString:String_val(user_id) encoding:NSASCIIStringEncoding];
 	[[MobileAppTracker sharedManager] setUserId:uid];
+	return Val_unit;
 }
 
 
-void ml_MATinstall(value unit) {
+value ml_MATinstall(value unit) {
+	if (!initialized) caml_failwith("MobileAppTracker not initialized");
 	[[MobileAppTracker sharedManager] trackInstall];
+	return Val_unit;
 }
 
-void ml_MATupdate(value unit) {
+value ml_MATupdate(value unit) {
+	if (!initialized) caml_failwith("MobileAppTracker not initialized");
 	[[MobileAppTracker sharedManager] trackUpdate];
+	return Val_unit;
 }
+
+value ml_MATtrackAction(value eventID) {
+	if (!initialized) caml_failwith("MobileAppTracker not initialized");
+	NSString *ns_eventID = [NSString stringWithCString:String_val(eventID) encoding:NSASCIIStringEncoding];
+	[[MobileAppTracker sharedManager] trackActionForEventIdOrName:ns_eventID eventIsId:YES];
+	return Val_unit;
+}
+
+
+value ml_MATtrackPurchase(value eventID,value amount,value currency) {
+	if (!initialized) caml_failwith("MobileAppTracker not initialized");
+	NSString *ns_eventID = [NSString stringWithCString:String_val(eventID) encoding:NSASCIIStringEncoding];
+	NSString *currencyCode = [NSString stringWithCString:String_val(currency) encoding:NSASCIIStringEncoding];
+	[[MobileAppTracker sharedManager] trackActionForEventIdOrName:ns_eventID eventIsId:YES revenueAmount:Double_val(amount) currencyCode:currencyCode];
+	return Val_unit;
+};
