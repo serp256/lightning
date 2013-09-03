5c0b42cb94c520e606ba4906f3095a2c72b6e829
SerP <serp256@gmail.com>
2013-09-03 15:27:39 +0400

    fix macos compilation

--- src/plugins/instagram/Instagram.ml
+++ src/plugins/instagram/Instagram.ml
@@ -2,6 +2,6 @@ IFPLATFORM(android ios)
 external installed: unit -> bool = "ml_instagram_installed";
 external post: ~fname:string -> ~text:string -> unit -> bool = "ml_instagram_post";
 ELSE
-value instaled () = False;
+value installed () = False;
 value post ~fname ~text () = False;
 ENDPLATFORM;
