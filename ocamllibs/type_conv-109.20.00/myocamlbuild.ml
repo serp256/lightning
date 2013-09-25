e1d4d411ddaf7a96872d220c41196eab5cc91ab0
nazarov.andrey <nazarov.andrey@me.com>
2013-09-25 08:29:35 +0400

    type_conv and sexplib replaced by clean tarballs content

--- ocamllibs/type_conv-109.20.00/myocamlbuild.ml
+++ ocamllibs/type_conv-109.20.00/myocamlbuild.ml
@@ -1,7 +1,7 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 947a507c5fc3823a5f3ebf1c7849804e) *)
+(* DO NOT EDIT (digest: c4be11b7bd681e5e48bf228377f8db04) *)
 module OASISGettext = struct
-(* # 21 "/mnt/local/sda1/jdimino/dot-opam/4.01.0dev+short-paths/build/oasis.0.3.0/src/oasis/OASISGettext.ml" *)
+(* # 21 "/home/ysulsky/local/opam-full/4.00.1/build/oasis.0.3.0/src/oasis/OASISGettext.ml" *)
 
   let ns_ str =
     str
@@ -24,7 +24,7 @@ module OASISGettext = struct
 end
 
 module OASISExpr = struct
-(* # 21 "/mnt/local/sda1/jdimino/dot-opam/4.01.0dev+short-paths/build/oasis.0.3.0/src/oasis/OASISExpr.ml" *)
+(* # 21 "/home/ysulsky/local/opam-full/4.00.1/build/oasis.0.3.0/src/oasis/OASISExpr.ml" *)
 
 
 
@@ -116,7 +116,7 @@ end
 
 # 117 "myocamlbuild.ml"
 module BaseEnvLight = struct
-(* # 21 "/mnt/local/sda1/jdimino/dot-opam/4.01.0dev+short-paths/build/oasis.0.3.0/src/base/BaseEnvLight.ml" *)
+(* # 21 "/home/ysulsky/local/opam-full/4.00.1/build/oasis.0.3.0/src/base/BaseEnvLight.ml" *)
 
   module MapString = Map.Make(String)
 
@@ -214,7 +214,7 @@ end
 
 # 215 "myocamlbuild.ml"
 module MyOCamlbuildFindlib = struct
-(* # 21 "/mnt/local/sda1/jdimino/dot-opam/4.01.0dev+short-paths/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
+(* # 21 "/home/ysulsky/local/opam-full/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
 
   (** OCamlbuild extension, copied from 
     * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
@@ -323,7 +323,7 @@ module MyOCamlbuildFindlib = struct
 end
 
 module MyOCamlbuildBase = struct
-(* # 21 "/mnt/local/sda1/jdimino/dot-opam/4.01.0dev+short-paths/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
+(* # 21 "/home/ysulsky/local/opam-full/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   (** Base functions for writing myocamlbuild.ml
       @author Sylvain Le Gall
@@ -339,7 +339,7 @@ module MyOCamlbuildBase = struct
   type name = string 
   type tag = string 
 
-(* # 56 "/mnt/local/sda1/jdimino/dot-opam/4.01.0dev+short-paths/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
+(* # 56 "/home/ysulsky/local/opam-full/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   type t =
       {
