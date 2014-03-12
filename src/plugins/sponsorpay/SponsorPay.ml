ba917e8613b9551c0888710f5e08b811e26997dc
nazarov.andrey <nazarov.andrey@me.com>
2014-03-12 16:16:30 +0400

    another minor fix

--- src/plugins/sponsorpay/SponsorPay.ml
+++ src/plugins/sponsorpay/SponsorPay.ml
@@ -5,12 +5,21 @@ IFPLATFORM(android ios)
 external start: ~appId:string -> ?userId:string -> ?securityToken:string -> ?networks:list network -> unit -> unit = "ml_sponsorPay_start";
 external showOffers: unit -> unit = "ml_sponsorPay_showOffers";
 
-external requestVideo: ~callback:(bool -> unit) -> unit -> unit = "ml_request_video";
-external showVideo: ~callback:(unit -> unit) -> unit -> unit = "ml_show_video";
 ELSE
 
 value start: ~appId:string -> ?userId:string -> ?securityToken:string -> ?networks:list network -> unit -> unit = fun ~appId ?userId ?securityToken ?mediatedNetworks () -> ();
 value showOffers () = ();
+
+ENDPLATFORM;
+
+IFPLATFORM(ios)
+
+external requestVideo: ~callback:(bool -> unit) -> unit -> unit = "ml_request_video";
+external showVideo: ~callback:(unit -> unit) -> unit -> unit = "ml_show_video";
+
+ELSE
+
 value requestVideo ~callback () = ();
 value showVideo ~callback () = ();
+
 ENDPLATFORM;
