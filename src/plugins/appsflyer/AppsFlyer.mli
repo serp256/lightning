3954355ca9c779e51f5a47f6e40b386936edd864
nazarov.andrey <nazarov.andrey@me.com>
2014-05-22 15:59:20 +0400

    appsflyer sdk up on ios

--- src/plugins/appsflyer/AppsFlyer.mli
+++ src/plugins/appsflyer/AppsFlyer.mli
@@ -4,7 +4,7 @@
  * Android: "Dev_id"
  * IOS: "App_ID;Dev_Key"
  *)
-value init: string -> unit;
+value init: ~appid:string -> ~devkey:string -> unit;
 value setUserId: string -> unit;
 value setCurrencyCode: string -> unit;
 value sendTracking: unit -> unit;
--- src/plugins/appsflyer/ios/af_ios.m
+++ src/plugins/appsflyer/ios/af_ios.m
@@ -1,53 +1,42 @@
-
-
-
-#import "ios/AppsFlyer.h"
+#import "ios/AppsFlyerTracker.h"
 #import <caml/mlvalues.h>
+#import <caml/alloc.h>
 #import <caml/fail.h>
 
-static NSString *appID = nil;
+value ml_af_set_key(value appid, value devkey) {
+	[AppsFlyerTracker sharedTracker].appsFlyerDevKey = [NSString stringWithUTF8String:String_val(devkey)];
+	[AppsFlyerTracker sharedTracker].appleAppID = [NSString stringWithUTF8String:String_val(appid)];
 
-
-
-
-value ml_af_set_key(value key) {
-	appID = [[NSString alloc] initWithCString:String_val(key) encoding:NSASCIIStringEncoding];
 	return Val_unit;
 }
 
-
-
 value ml_af_set_user_id(value uid) {
-	NSString *nsuid = [NSString stringWithCString:String_val(uid) encoding:NSASCIIStringEncoding];
-	[AppsFlyer setAppUID:nsuid];
+	[AppsFlyerTracker sharedTracker].customerUserID = [NSString stringWithUTF8String:String_val(uid)];
 	return Val_unit;
 }
 
 
 value ml_af_get_uid(value p) {
-	NSString *uid = [AppsFlyer getAppsFlyerUID];
+	NSString *uid = [AppsFlyerTracker sharedTracker].customerUserID;
 	return caml_copy_string([uid UTF8String]);
 }
 
 value ml_af_set_currency_code(value code) {
-	NSString *nscode = [NSString stringWithCString:String_val(code) encoding:NSASCIIStringEncoding];
-	[AppsFlyer setCurrencyCode:nscode];
+	[AppsFlyerTracker sharedTracker].currencyCode = [NSString stringWithUTF8String:String_val(code)];
 	return Val_unit;
 }
 
 
 value ml_af_send_tracking(value p) {
-	if (!appID) caml_failwith("AppsFlyer not initialized");
-	[AppsFlyer notifyAppID:appID];
+	[[AppsFlyerTracker sharedTracker] trackAppLaunch];
 	return Val_unit;
 }
 
 
 value ml_af_send_tracking_with_event(value evkey,value evval) {
-	if (!appID) caml_failwith("AppsFlyer not initialized");
-	NSLog(@"send_tracking_with_event");
 	NSString *nsev = [NSString stringWithCString:String_val(evkey) encoding:NSASCIIStringEncoding];
 	NSString *nsval = [NSString stringWithCString:String_val(evval) encoding:NSASCIIStringEncoding];
-	[AppsFlyer notifyAppID:appID event:nsev eventValue:nsval];
+	[[AppsFlyerTracker sharedTracker] trackEvent:nsev withValue:nsval];
+
 	return Val_unit;
 }
