98edc06cfe94e38f8f44c4a8d48114931af0498a
nazarov.andrey <nazarov.andrey@me.com>
2014-09-12 15:41:42 +0400

    ios vk sdk update

--- src/plugins/vk/ios/LightVkDelegate.m
+++ src/plugins/vk/ios/LightVkDelegate.m
@@ -14,51 +14,6 @@
 	return self;
 }
 
-// - (void)vkSdkDidAcceptUserToken:(VKAccessToken *)token
-// {
-// 	NSLog(@"!!!vkSdkDidAcceptUserToken");
-// }
-//
-// - (void)vkSdkDidReceiveNewToken:(VKAccessToken *)newToken
-// {
-// 	NSLog(@"!!!vkSdkDidReceiveNewToken");
-// 	*authorized = 1;
-// 	RUN_CALLBACK(success, Val_unit);
-// }
-//
-// - (void)vkSdkDidRenewToken:(VKAccessToken *)newToken
-// {
-// 	NSLog(@"!!!vkSdkDidRenewToken");
-// }
-//
-// - (void)vkSdkNeedCaptchaEnter:(VKError *)captchaError
-// {
-// 	NSLog(@"!!!vkSdkNeedCaptchaEnter");
-// }
-//
-// - (void)vkSdkShouldPresentViewController:(UIViewController *)controller
-// {
-// 	NSLog(@"!!!vkSdkShouldPresentViewController");
-// }
-//
-// - (void)vkSdkTokenHasExpired:(VKAccessToken *)expiredToken
-// {
-// 	NSLog(@"!!!vkSdkTokenHasExpired");
-// }
-//
-// - (void)vkSdkUserDeniedAccess:(VKError *)authorizationError
-// {
-// 	NSLog(@"!!!vkSdkUserDeniedAccess");
-// 	*authorized = 0;
-// 	RUN_CALLBACK(fail, caml_copy_string([[NSString stringWithFormat:@"%@: %@", authorizationError.errorMessage, authorizationError.errorReason ] UTF8String]));
-// }
-//
-// - (void)dealloc
-// {
-// 	FREE_CALLBACK(success);
-// 	FREE_CALLBACK(fail);
-// }
-
 - (void)vkSdkNeedCaptchaEnter:(VKError *)captchaError {
 
 }
@@ -76,17 +31,17 @@
 
 }
 
-- (void)vkSdkDidReceiveNewToken:(VKAccessToken *)newToken {
+- (void)vkSdkReceivedNewToken:(VKAccessToken *)newToken {
 	*authorized = 1;
 	[newToken saveTokenToDefaults:@"lightning_nativevk_token"];
 	RUN_CALLBACK(success, Val_unit);
-}
-
-- (void)vkSdkDidAcceptUserToken:(VKAccessToken *)token {
 
 }
 
-- (void)vkSdkDidRenewToken:(VKAccessToken *)newToken {
+- (void)vkSdkAcceptedUserToken:(VKAccessToken *)token {
+
+}
+- (void)vkSdkRenewedToken:(VKAccessToken *)newToken {
 	[newToken saveTokenToDefaults:@"lightning_nativevk_token"];
 }
 
