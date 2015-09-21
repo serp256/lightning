5b653b48e0a36bee346bf6868b57b065ffc4ca68
Olga Borzenkova <kyssike@gmail.com>
2015-09-21 11:05:21 +0300

    keyboard fix for android tv

--- src/android/main_android.h
+++ src/android/main_android.h
@@ -3,6 +3,10 @@
 
 #include "engine_android.h"
 
+#define NOT_HANDLED 0
+#define HANDLED 1
+// need to handle controller joysticks movement for tv keyboard
+#define HANDLED_NOT_HANDLED 2
 void android_main(struct android_app* state);
 
 #endif
