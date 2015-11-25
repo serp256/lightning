00326b272f6d01ff6a2d51bde2c45b29fec0b0a9
Olga Borzenkova <kyssike@gmail.com>
2015-11-25 18:21:07 +0300

    some meta fixes

--- src/plugins/gameanalytics/Gameanalytics.ml
+++ src/plugins/gameanalytics/Gameanalytics.ml
@@ -1,4 +1,8 @@
 
+type resourceEventType = [= `sink| `source];
+type progressionEventType = [=`start | `complete | `fail];
+type errorEventType = [=`edebug | `info | `warning | `error | `critical];
+
 IFPLATFORM(android)
 
 external _init: string -> string -> option string -> bool -> option (list string)-> option (list string) -> option (list string) -> unit = "ml_ga_init_byte" "ml_ga_init";
@@ -7,30 +11,27 @@ value init ~appKey ~secretKey ?version ?(enableDebug=False) ?currencies ?itemTyp
 external _businessEvent: string -> string -> string -> string -> int -> string -> string -> unit = "ml_ga_business_event_byte" "ml_ga_business_event";
 value businessEvent ~cartType ~itemType ~itemId ~currency ~amount ~receipt ~signature () = _businessEvent cartType itemType itemId currency amount receipt signature;
 
-type resourceEventType = [= `sink| `source];
 external _resourceEvent: resourceEventType -> string -> float -> string -> string -> unit = "ml_ga_resource_event";
 value resourceEvent ~flowType ~currency ~amount ~itemType ~itemId () = _resourceEvent flowType currency amount itemType itemId;
 
-type progressionEventType = [=`start | `complete | `fail];
 external _progressionEvent: progressionEventType -> string -> option string -> option string -> option int -> unit = "ml_ga_progression_event";
 value progressionEvent ~status ~progression1 ?progression2  ?progression3  ?score () = _progressionEvent status progression1 progression2 progression3 score;
 
 external _designEvent: string -> option float -> unit  = "ml_ga_design_event";
 value designEvent ~eventId ?fvalue () = _designEvent eventId fvalue;
 
-type errorEventType = [=`edebug | `info | `warning | `error | `critical];
 external _errorEvent: errorEventType -> option string -> unit  = "ml_ga_error_event";
 value errorEvent ~errorType ?message () = _errorEvent errorType message;
 
 ELSE
 
-value init ~appKey ~secretKey ?version ?(enableDebug=False) ?currencies ?itemTypes ?dimensions () = fun _ _ _ _ _ _ _ _ -> ();
-value businessEvent ~cartType ~itemType ~itemId ~currency ~amount ~receipt ~signature () = fun _ _ _ _ _ _ _ _ -> ();
-value resourceEvent ~flowType ~currency ~amount ~itemType ~itemId () = fun _ _ _ _ _ _ -> ();
-value progressionEvent ~status ~progression1 ?progression2  ?progression3  ?score () = fun _ _ _ _ _ _ -> ();
+value init ~appKey ~secretKey ?version ?(enableDebug=False) ?currencies ?itemTypes ?dimensions () = ();
+value businessEvent ~cartType ~itemType ~itemId ~currency ~amount ~receipt ~signature () =  ();
+value resourceEvent ~flowType ~currency ~amount ~itemType ~itemId () = ();
+value progressionEvent ~status ~progression1 ?progression2  ?progression3  ?score () = ();
 
-value designEvent ~eventId ?fvalue () = fun _ _ _ -> ();
-value errorEvent ~errorType ?message () = fun _ _ _ -> ();
+value designEvent ~eventId ?fvalue () = ();
+value errorEvent ~errorType ?message () = ();
 
 ENDPLATFORM;
 
