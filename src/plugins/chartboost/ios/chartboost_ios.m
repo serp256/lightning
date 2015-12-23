9bf9465a11a62dcb8cbbd0fe29c937605378d2d3
pushkin <pushkin666@inbox.ru>
2015-12-23 10:51:15 +0300

    remove old chartboost from ios

--- src/plugins/chartboost/ios/chartboost_ios.m
+++ src/plugins/chartboost/ios/chartboost_ios.m
@@ -1,10 +1,9 @@
 #import "caml/mlvalues.h"
-#import "Chartboost.h"
 
 void ml_chartBoostStartSession(value v_appId, value v_appSig) {
 	static int started = 0;
 	if (started) return;
-/*
+	/*
 	Chartboost* cb = [Chartboost sharedChartboost];
     cb.appId = [NSString stringWithCString:String_val(v_appId) encoding:NSASCIIStringEncoding];
     cb.appSignature = [NSString stringWithCString:String_val(v_appSig) encoding:NSASCIIStringEncoding];
