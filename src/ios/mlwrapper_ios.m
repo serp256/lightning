e1d511b7be2cea1e89100f5a55eeb0cd27899c64
Olga Borzenkova <kyssike@gmail.com>
2016-02-08 16:35:30 +0300

    ios system font path

--- src/ios/mlwrapper_ios.m
+++ src/ios/mlwrapper_ios.m
@@ -653,3 +653,24 @@ value ml_disableAwake(value unit) {
     [UIApplication sharedApplication].idleTimerDisabled = NO;
     return Val_unit;
 }
+
+value ml_getSystemFontPath(value unit) {
+   // [UIApplication sharedApplication].idleTimerDisabled = NO;
+	NSArray *familyNames = [[NSArray alloc] initWithArray:[UIFont familyNames]];
+	  NSArray *fontNames;
+		  NSInteger indFamily, indFont;
+			  for (indFamily=0; indFamily<[familyNames count]; ++indFamily)
+					  {
+							      NSLog(@"Family name: %@", [familyNames objectAtIndex:indFamily]);
+										      fontNames = [[NSArray alloc] initWithArray:
+														          [UIFont fontNamesForFamilyName:
+																			          [familyNames objectAtIndex:indFamily]]];
+													      for (indFont=0; indFont<[fontNames count]; ++indFont)
+																	      {
+																					          NSLog(@"    Font name: %@", [fontNames objectAtIndex:indFont]);
+																										      }
+																      [fontNames release];
+																			  }
+				  [familyNames release];
+    return "System/Font";
+}
